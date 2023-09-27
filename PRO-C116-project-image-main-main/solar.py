1fc3ee6b858c7377d72afbac5b9150458be178e2
Rhea0909 <98905963+Rhea0909@users.noreply.github.com>
2023-09-27 17:54:40 +0100

    Add files via upload

---
+++ PRO-C116-project-image-main-main/solar.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread("solar-system.jpg")
+cv2.putText(img, "Sun", (100,80), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0,0,255))
+cv2.putText(img, "Mercury", (110,180), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.putText(img, "Venus", (190,270), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.putText(img, "Earth", (300,270), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.putText(img, "Mars", (400,270), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.putText(img, "Jupiter", (500,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.putText(img, "Saturn", (720,130), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.putText(img, "Uranus", (930,270), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.putText(img, "Neptune", (1100,100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255))
+cv2.imshow("output", img)
+cv2.waitKey(0)
+cv2.imwrite("Solar sytem with name.jpg",img)
